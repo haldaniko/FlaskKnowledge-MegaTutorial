fc86ef72d2d77375f35bcff91b43c9ba4d125e64
haldaniko <haldaniko@gmail.com>
2023-04-12 11:08:11 +0200

    Ver 0.2 - Added base.html

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,27 @@
+from flask import render_template
 from app import app
+import random
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'miguel'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    number = random.randint(0, 1000)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts,
+                           number=number)
 
