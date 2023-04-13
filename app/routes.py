ceeb3c3cee95f5c1dd8965a2e821943c04baf6b8
haldaniko <haldaniko@gmail.com>
2023-04-13 19:55:15 +0200

    Ver 0.3

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 import random
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('User: {}'.format(form.username.data))
+        flash('Password {}'.format(form.password.data))
+        flash('remember_me={}'.format(form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/')
